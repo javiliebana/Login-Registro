b0338f6ce1bb0872eebdeec578d6b2f225fc7538
javier <javierliebana99@gmail.com>
2018-10-31 19:36:29 +0100

    1º commit

---
+++ 18-10-31 LOGIN/src/application/newController.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class newController {
+	
+	private Stage stage;
+	@FXML
+	private void handleClose() {
+		System.exit(0);
+	}
+	@FXML
+	private void nuevaVista() {
+		try {
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(getClass().getResource("v_registro.fxml"));
+	        AnchorPane mypane = (AnchorPane) loader.load();
+	        Scene scene= new Scene(mypane);
+	        stage = new Stage();
+	        stage.setTitle("New Window");
+	        stage.setScene(scene);
+	        stage.show();
+  
+	    } catch (IOException e) {
+	        
+	    }
+	}
+}
